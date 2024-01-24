c7eca0b72bed8bdeb49ca35ffc1318965af024f1
Султон.Абиев <SAbiyev@courtyardtashkent.uz>
2024-01-24 17:30:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let username = document.querySelector(".username");
+let lastname = document.querySelector(".lastname");
+let email = document.querySelector(".email");
+let password1 = document.querySelector(".password");
+let password2 = document.querySelectorAll(".password")[1];
+
+let btn = document.querySelector(".btn");
+
+btn.addEventListener('click', clickMe);
+
+let userNameReg = /^[A-Za-zА-Яа-я\- ]*[^0-9][A-Za-zА-Яа-я\- ]*$/;
+let userEmailReg= /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+let userPasswordReg= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+
+function clickMe(event) {
+    event.preventDefault();
+    validate(userNameReg, username);
+    validate(userNameReg, lastname);
+    validate(userEmailReg, email);
+    validate(userPasswordReg, password1);
+    validate(userPasswordReg, password2);
+}
+
+function validate(userReg, userValue) {
+    if (!userReg.test(userValue.value)) {
+        userValue.classList.add("error");
+    }
+}
